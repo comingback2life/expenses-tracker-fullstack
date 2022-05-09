bd39f99d9be637a69b4af2b9359d991317b1c2a7
Samip <callmesamip@gmail.com>
2022-05-10 09:41:58 +1000

    Pages, Components and Routes being setup

--- expenses-tracker-frontend/src/App.js
+++ expenses-tracker-frontend/src/App.js
@@ -1,9 +1,18 @@
-
+import {Routes,Route, BrowserRouter} from 'react-router-dom';
+import { Dashboard } from './pages/Dashboard';
+import { Login } from './pages/Login';
+import { Register } from './pages/Register';
 
 function App() {
   return (
-    <div className="app">
-      Hello World
+  <div>
+    <BrowserRouter>
+    <Routes>
+      <Route path ='/' element={<Login/>}/>
+      <Route path ='/register' element={<Register/>}/>
+      <Route path ='/dashboard' element={<Dashboard/>}/>
+    </Routes>
+    </BrowserRouter>
     </div>
   );
 }
