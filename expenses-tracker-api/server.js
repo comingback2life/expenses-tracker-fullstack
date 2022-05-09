ace37ee376f96f4901e39632a198c492ad628c39
Samip <callmesamip@gmail.com>
2022-05-09 14:42:37 +1000

    Imported userRouter,morgan and other required libraries in server file

--- expenses-tracker-api/server.js
+++ expenses-tracker-api/server.js
@@ -1,14 +1,29 @@
 import express from 'express';
+
 const app = express();
 const PORT = process.env.PORT || 8000; //use the port from process env or if falsey value then use 8000.
 
+//Middleware
+import cors from 'cors';
+import morgan from 'morgan';
 
 
-app.get('*',(req,res)=>{
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(morgan('tiny'));
+
+//Import DB connection 
+import { connectDB } from './src/config/db.js';
+connectDB();
+
+import userRouter from './src/routers/userRouter.js';
+app.use('/api/v1/user', userRouter);
+app.get('*', (req, res) => {
   res.status(404).send(`<h1>404, not found</h1>`);
-})
+});
 
-app.listen(PORT,(error)=>{
+app.listen(PORT, (error) => {
   error && console.log(error); //if error > log Error
   console.log(`Server is running on ${PORT}`);
-})
+});
