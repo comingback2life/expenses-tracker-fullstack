a00272f010fcd53fcd0599e7619363780eab5f2b
Samip <callmesamip@gmail.com>
2022-05-16 15:05:59 +1000

    State being updated now

--- expenses-tracker-frontend/src/pages/Dashboard.js
+++ expenses-tracker-frontend/src/pages/Dashboard.js
@@ -3,7 +3,7 @@ import { MainLayout } from '../components/Layout/MainLayout';
 import { useNavigate } from 'react-router-dom';
 import { ExpensesForm } from '../components/expenses-form/ExpensesForm';
 import { ExpensesTable } from '../components/expenses-form/ExpensesTable';
-import { getUserExpenses, postExpenses } from '../components/helpers/axiosHelper';
+import { getUserExpenses, postExpenses,deleteExpense } from '../components/helpers/axiosHelper';
 import { Alert, Col, Row, Spinner } from 'react-bootstrap';
 export const Dashboard = () => {
   const [response, setResponse] = useState({
@@ -40,6 +40,10 @@ export const Dashboard = () => {
       const {data} = await getUserExpenses();
       data?.status==='success' && setExpensesList(data.getUserExpenses)
   }
+  const handleOnDeleteClick= async (postID)=>{
+    const {data} = await deleteExpense(postID);
+    data ?.status==="success" && fetchExpenses();
+  }
   return (
     <MainLayout>
       <h4 className='display-7 mt-5 mb-3'>Dashboard</h4>
@@ -56,7 +60,7 @@ export const Dashboard = () => {
         </Col>
       </Row>
       <ExpensesForm handleOnPostData={handleOnPostData}/>
-      <ExpensesTable expensesList={expensesList}/>
+      <ExpensesTable expensesList={expensesList} handleOnDeleteClick={handleOnDeleteClick}/>
     </MainLayout>
   )
 }
